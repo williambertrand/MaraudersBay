2a304b4bd4ffd51e96f9b59e38745177291f17e4
William <william.bertrand94@gmail.com>
2022-07-23 19:40:40 -0700

    simple boat firing

---
+++ Assets/Scripts/Player/ShipFiring.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipFiring : MonoBehaviour
+{
+
+    public enum Side
+    {
+        PORT,
+        STARBOARD
+    }
+
+    [SerializeField] private Transform PORT_Firing;
+    [SerializeField] private Transform STARBOARD_Firing;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float firingPower;
+
+    [SerializeField] private float reloadTime;
+    private float lastFireTime;
+
+    [SerializeField] private float accuracyValue;
+
+    private Side toFireSide = Side.PORT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FireCannon()
+    {
+
+        if (Time.time - lastFireTime <= reloadTime) return;
+
+        Transform initPoint = toFireSide == Side.PORT ? PORT_Firing : STARBOARD_Firing;
+
+        GameObject proj = Instantiate(projectile, initPoint.position, Quaternion.identity);
+
+        Rigidbody rb = proj.GetComponent<Rigidbody>();
+
+
+        Vector3 accuracyOffset = new Vector3(
+            Random.Range(-accuracyValue, accuracyValue),
+            Random.Range(-accuracyValue, accuracyValue),
+            0.0f
+        );
+
+        Vector3 aim = initPoint.transform.forward + accuracyOffset;
+
+        rb.AddForce(firingPower * aim);
+
+        // Swap firing to other side
+        toFireSide = toFireSide == Side.PORT ? Side.STARBOARD : Side.PORT;
+
+        lastFireTime = Time.time;
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+
+        switch(context.phase)
+        {
+            case InputActionPhase.Performed:
+                FireCannon();
+                break;
+            default:
+                break;
+        }
+
+      
+    }
+
+}
---
+++ Assets/Scripts/Projectiles/BasicProjectile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicProjectile : MonoBehaviour
+{
+
+    // Destroy after ball has lived this long
+    [SerializeField] private float TTL;
+
+    // Destroy after ball has sunk below this line
+    [SerializeField] private float MIN_DEPTH = -5;
+
+    private float? createdAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createdAt = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (createdAt != null)
+        {
+            if (Time.time - createdAt >= TTL)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (transform.position.y <= MIN_DEPTH)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // TODO: On colliding with enemy ship, deal damage
+}
