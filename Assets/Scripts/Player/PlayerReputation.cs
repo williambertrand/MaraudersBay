066c4c5cf437d0da1fa1562c47567a421e39b952
William <william.bertrand94@gmail.com>
2022-07-29 17:55:04 -0600

    Player gold ui, player reputation ui

---
+++ Assets/Scripts/Player/PlayerReputation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerReputation : MonoBehaviour
+{
+    // Reputation related fields and UI
+    [SerializeField] private int startReputation;
+    public int playerCurrentReputation;
+    [SerializeField] private TMP_Text playerReputationText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCurrentReputation = startReputation;
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void updateReputation(int amount)
+    {
+        playerCurrentReputation += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (playerReputationText != null)
+        {
+            playerReputationText.text = "" + playerCurrentReputation;
+        }
+    }
+}
