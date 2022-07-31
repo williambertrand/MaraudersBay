265576f946c01889fb9197082f4f7d7fe68d0cda
William <william.bertrand94@gmail.com>
2022-07-31 17:17:35 -0600

    Docking basics and ui set up!

---
+++ Assets/Scripts/Docking/Dock.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DockType
+{
+    OUTPOST,
+    ISLAND
+}
+
+public class Dock : MonoBehaviour
+{
+
+    [SerializeField] private DockType dockType;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UIControlls ui = other.gameObject.GetComponent<UIControlls>();
+            ui.EnableDockingOption(dockType);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UIControlls ui = other.gameObject.GetComponent<UIControlls>();
+            ui.DisableDockingOption();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDockingControls.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public struct DockUI
+{
+    public GameObject UI;
+    public DockType forDockType;
+}
+
+
+public class PlayerDockingControls : MonoBehaviour
+{
+
+    private ShipFiring firing;
+    private ShipMovement movement;
+    Rigidbody rigidBody;
+
+    public List<DockUI> DockingUis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firing = GetComponent<ShipFiring>();
+        movement = GetComponent<ShipMovement>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void Dock(DockType type)
+    {
+        firing.enabled = false;
+        movement.enabled = false;
+        rigidBody.velocity = Vector3.zero;
+
+        DockUI uiToShow = DockingUis.Find(ui => ui.forDockType == type);
+        uiToShow.UI.SetActive(true);
+    }
+
+    public void UnDock(DockType type)
+    {
+        firing.enabled = true;
+        movement.enabled = true;
+
+        DockUI uiToHide= DockingUis.Find(ui => ui.forDockType == type);
+        uiToHide.UI.SetActive(false);
+    }
+}
