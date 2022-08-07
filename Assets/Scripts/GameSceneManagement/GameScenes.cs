ba4947927e054db3ce778da0ea66a6052f2d5926
William <william.bertrand94@gmail.com>
2022-08-07 12:35:50 -0600

    Playfab auth (#14)
    * login and sign up to play
    
    * hide playfab token from git

---
+++ Assets/Scripts/GameSceneManagement/GameScenes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameScenes
+{
+    public static int Auth = 0;
+    public static int Menu = 2; // TODO: No menu screen currently
+    public static int GamePlay = 1;
+}
